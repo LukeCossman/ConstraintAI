3ab6b739ef0563dad4db7e7e133dea3edf3a9b87
Luke Cossman <lc2027@desales.edu>
2022-12-16 21:47:02 -0500

    WORKS

--- scheduler.py
+++ scheduler.py
@@ -5,11 +5,13 @@ Created on Sat Nov 26 18:52:47 2022
 @author: majes
 """
 
-#What's so funny about sussus amogus
+
 
 import pandas as pd
 import random as r
 import os
+import time
+
 
 class course():
     def __init__(self, name="", professor="", day="", room=0, conflicts=[], time=0):
@@ -29,15 +31,16 @@ class course():
             return True
         else:
             return False
+
                 
         
 if __name__ == "__main__":
-     
+    
     ALL = []
     data = pd.read_csv("input.csv")
     
-    for i in range(len(data)):
-        row = data.iloc[i]
+    for x in range(len(data)):
+        row = data.iloc[x]
         
         name = row["Course"]
         professor = row["Professors"]
@@ -47,7 +50,9 @@ if __name__ == "__main__":
         
         ALL.append(course(name, professor, day, room, conflicts))
         
-        
+    a = data["Room"].value_counts()[223]
+    print(a)    
+    
     MWF = 0
     TTH = 1
     
@@ -60,10 +65,12 @@ if __name__ == "__main__":
     professors = {"Ryan" : 0, "Gupta" : 0, "Houck" : 0, "Hammarsten" : 0, "Ruggles" : 0,
                   "Walsh" : 0}
     
+    tries = 0
     while not solution:
         
         bad = True
         current = ALL[i]
+        
         while bad:
             time_error = False
             professor_error = False
@@ -72,55 +79,80 @@ if __name__ == "__main__":
             if current.day == "MWF":
                 
                 current.time = timeMWF[r.randint(0, 7)]
-                professors[current.professor] += 1
             
                 for course in schedule[MWF]:
-                    if course.time == current.time and course.room == current.time:
-                        time_error = True
-                    if course.time == current.time and course.name in current.conflicts:
-                        time_error = True
-                    if course.time == current.time and course.professor == current.professor:
-                        professor_error = True
+                    if course != current:
+                        if course.time == current.time and course.room == current.time:
+                            time_error = True
+                        if course.time == current.time and course.name in current.conflicts:
+                            time_error = True
+                        if course.time == current.time and course.professor == current.professor:
+                            professor_error = True
+                    
             
             
             else:
                 current.time = timeTTH[r.randint(0, 5)]
-                professors[current.professor] += 1
                 
                 for course in schedule[TTH]:
-                    if course.time == current.time and course.room == current.time:
-                        time_error = True
-                    if course.time == current.time and course.name in current.conflicts:
-                        time_error = True
-                    if course.time == current.time and course.professor == current.professor:
-                        professor_error = True
+                    if course != current:
+                        if course.time == current.time and course.room == current.room:
+                            time_error = True
+                        if course.time == current.time and course.name in current.conflicts:
+                            time_error = True
+                        if course.time == current.time and course.professor == current.professor:
+                            professor_error = True
+                    
+            
+            #If errors
+            if professor_error or time_error:
                 
-            
-            for day in schedule:
-                for p in professors:
-                    if professors[p] > 5:
-                        professor_error = True
-            
-            
-            if not (professor_error or time_error):
+                tries += 1
+                if tries == 10:
+                    tries = 0
+                    i -= 1
+                    if current.day == "MWF":
+                        schedule[MWF].pop(-1)
+                    else:
+                        schedule[TTH].pop(-1)
+                
+            #If no error
+            else:
                 bad = False
                 i += 1
+                tries = 0
                 if current.day == "MWF":
                     schedule[MWF].append(current)
                 else:
                     schedule[TTH].append(current)
+                
+            
             
         #if done
         if i == len(ALL):
             solution = True
             
+    d = {}
+    d["Course"] = []
+    d["Day"] = []
+    d["Time"] = []
+    d["Professor"] = []
+    d["Room"] = []
+    output = pd.DataFrame(d)
+    
             
     for day in schedule:
         for course in day:
-            print(course, course.day, course.time)
+            
+            output.loc[len(output.index)] = [course.name, course.day,
+                                             course.time, course.professor,
+                                             course.room]
+            
+    
+    print(output)
     
         
-     
+    
     
     
     
